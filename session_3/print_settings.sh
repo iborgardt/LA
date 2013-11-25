53c3a4611812cf414b16400d32b13b117bb8f44a
vasya pupkin <vasya@pupkin.com>
2013-11-25 18:41:26 +0400

    3.2 print_settings.sh is created.

---
+++ session_3/print_settings.sh
@@ -0,0 +1,7 @@
+source settings.conf
+cat << EndOfLine
+###########################################
+## Default login/password: $OS_USERNAME/$OS_PASSWORD
+## tenant: $OS_TENANT_NAME
+###########################################
+EndOfLine
