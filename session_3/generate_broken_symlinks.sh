f368cf8337ca279a4177fbbb1a7dd095fdb59306
pupkin <pupkin@ya.ru>
2013-11-27 11:28:05 +0400

    3.3b Modify generate_broken_symlinks.sh

--- session_3/generate_broken_symlinks.sh
+++ session_3/generate_broken_symlinks.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-touch not_ver.file
+work_dir=${1:-`pwd`}
+mkdir -p $work_dir || exit 1
+
+touch "$work_dir/not_ver.file"
 
 for i in `seq 0 10`; do
-	touch "ver$i.file"
+	touch "$work_dir/ver$i.file"
 done
 
-for fname in *.file; do
-	ln -s "$fname" "${fname%%.file}.lnk"
+for fname in "$work_dir"/*.file; do
+	ln -sf "$fname" "${fname%%.file}.lnk"
 	rm "$fname"
 done
