212bfc3a318e810646a0c93b17cdc3966147ec10
pupkin <pupkin@ya.ru>
2013-12-01 10:09:44 +0400

    4.1 Finished edit of assign.sh

--- session_4/assign.sh
+++ session_4/assign.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+: << COMMENTARY
+	This script reads the names and tasks from 
+	files $names and $tasks, after this it assigns
+	in the $work_table task to worker and puts 
+	in the $workers enumerated list of all the names
+	ordered by alphabet.
+	$workers and $work_table are specified in the settings.conf file.
+COMMENTARY
+
+source settings.conf
 names='names'
 tasks='tasks'
-workers='workers.lst'
-work_table='work.table'
 
-> "$workers"
 > "$work_table"
 i=1
 
 cat "$names" | sort -u | while read name; do
 
-	echo "[$i]: $name [${#name}]" >> $workers
+	echo "[$i]: $name [${#name}]"
 	((i++))
-done
+done > "$workers"
 
 echo "Current tasks:"
 cat "$tasks" | tr ' ' '_'
@@ -22,7 +29,7 @@ exec 3>&1
 exec > "$work_table"
 
 echo "Assigned tasks:"
-paste -d'=' $names $tasks
+paste -d'=' "$names" "$tasks"
 echo "Date: `date +%m/%d/%Y` Time: `date +%H:%M:%S`"
 exec 1>&3 3>&-
 
